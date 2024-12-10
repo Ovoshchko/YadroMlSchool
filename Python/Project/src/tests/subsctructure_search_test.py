8d2833407f3c4c0bdb906cbe734bb0f61dcd14cb
Овощко <97815443+Ovoshchko@users.noreply.github.com>
2024-12-10 23:34:12 +0300

    Second

--- Python/Project/src/tests/subsctructure_search_test.py
+++ Python/Project/src/tests/subsctructure_search_test.py
@@ -1,5 +1,5 @@
 import unittest
-from .src.molecular.rdkit_functions import substructure_search
+from molecular.rdkit_functions import substructure_search
 
 
 class TestSubstructureSearch(unittest.TestCase):
